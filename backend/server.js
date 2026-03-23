981875a0c1a4856a8945b2358e9cea90699f2492
AmitSharma <Sandy31524@gmail.com>
2026-03-23 22:39:26 +0530

    backendcode

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// routes
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
