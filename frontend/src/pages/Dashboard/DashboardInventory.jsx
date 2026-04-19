272b73e6ff893755c52940ee57c029e66745e6cf
AmitSharma <Sandy31524@gmail.com>
2026-04-19 19:44:31 +0530

    testing in inventory Dashboard

--- frontend/src/pages/Dashboard/DashboardInventory.jsx
+++ frontend/src/pages/Dashboard/DashboardInventory.jsx
@@ -4,16 +4,24 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 export const DashboardInventory = () => {
 
-  const [products,setProducts] = useState('');
+  const [products,setProducts] = useState([]);
 
   useEffect(()=>{
     (async() => {
       const products = await getProducts();
-      console.log(products);
+      setProducts(products)
     } )()
   },[])
 
   return <div>
-    
+    {products?.map((item,key) => (
+      <div key={key}>
+        <p>{item?.name}</p>
+        <p>{item?._id}</p>
+        <p>{item?.category}</p>
+        <p>description {item.description}</p>
+        <p>Rs.{item.price}</p>
+      </div>
+    ))}
   </div>;
 };
